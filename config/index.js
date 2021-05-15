78f0d9fc1452ddc630d3e61fe20f106e4ac0be12
Carlos Guzmán <cags84@gmail.com>
2021-05-15 08:31:18 -0500

    Fix - eslint, eslint-config-prettier
    - Add eslint, eslint-config-prettier to package.json
    - Fix listen address to 127.0.0.1

--- config/index.js
+++ config/index.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 const config = {
   http: {
-    host: process.env.HTTP_HOST || "0.0.0.0",
+    host: process.env.HTTP_HOST || "127.0.0.1",
     port: process.env.PORT || process.env.HTTP_PORT,
   },
   log: {

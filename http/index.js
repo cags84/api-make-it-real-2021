f7c6435a1cb195ea89f9dd6ddbe57a1582112470
Carlos Guzmán <cags84@gmail.com>
2021-05-15 10:06:13 -0500

    fix cors

--- http/index.js
+++ http/index.js
@@ -5,9 +5,21 @@ const api = require("../api");
 const { config } = require("../config");
 
 const { host, port } = config.http;
+const corsOptions = {
+  // Para la cookie sea aceptada en el navegador es importante creat un alias
+  // en el host de windows, para no rechace este tipo de peticiones
+  // mas info https://www.liquidweb.com/kb/edit-host-file-windows-10/
+  origin: [
+    "http://127.0.0.1",
+    "http://127.0.0.1:5500",
+    "http://api.makeitreal.local:5500"
+  ],
+  credentials: true,
+  exposeHeaders: ["set-cookie"],
+};
 
 const app = express();
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api", api);
